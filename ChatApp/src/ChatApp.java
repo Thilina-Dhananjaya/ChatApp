e116d91f703935126fce21d76efe2dbea33a6b9a
Thilina-Dhananjaya <120640837+Thilina-Dhananjaya@users.noreply.github.com>
2023-09-29 11:56:05 +0530

    middle

---
+++ ChatApp/src/ChatApp.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author UserA
+ */
+public class ChatApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        LoginForm lgf = new LoginForm();
+        lgf.setVisible(true);
+        lgf.pack();
+        lgf.setLocationRelativeTo(null);
+
+// TODO code application logic here
+    }
+    
+}
